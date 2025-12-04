cbddabda48caf0fbc621afdcd768078b5d00af9f
janeneos <janeneos@student.21-school.ru>
2025-12-04 18:11:29 +0300

    init commit

---
+++ src/test/java/ru/mephi/AppTest.java
@@ -0,0 +1,12 @@
+package ru.mephi;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testGetGreeting() {
+        App app = new App();
+        assertEquals("Hello, World!", app.getGreeting());
+    }
+}
